0126bbab8f93b1d6837e0dc984ba415988de0ed7
Cassio Andrade <andrade.cassio@hotmail.com>
2023-09-30 16:28:56 -0300

    inserindo mudanca pelo VS

--- app_em_javascript.js
+++ app_em_javascript.js
@@ -1,3 +1,4 @@
 console.log("Rodando sistema de gerenciamento de jogos ")
 console.log("Mudança feita por andradecassiousp")
 console.log("Mudança2 feita por andradecassiousp")
+console.log("Mudanças pelo VS Code")
